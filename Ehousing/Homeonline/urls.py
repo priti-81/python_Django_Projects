635e95b21d551ba06d5831cf0eef1a4a5fca6af1
priti-81 <preetisharma.1897@gmail.com>
2023-08-09 19:07:41 +0530

    static

--- Ehousing/Homeonline/urls.py
+++ Ehousing/Homeonline/urls.py
@@ -3,4 +3,13 @@ from Homeonline import views
 
 urlpatterns = [
    path('',views.index),
+   path('about/',views.about),
+   path('bloggrid/',views.bloggrid),
+   path('buy/',views.buy),
+   path('rent/',views.rent),
+   path('sell/',views.sell),
+   path('update/',views.update),
+   path('contact/',views.contact),
+   path('signup/',views.signup,name='signup'),
+   path('login/',views.login,name='login'),
 ]
--- Ehousing/Homeonline/views.py
+++ Ehousing/Homeonline/views.py
@@ -3,3 +3,30 @@ from django.shortcuts import render
 # Create your views here.
 def index(request):
     return render(request,'index.html')
+
+def signup(request):
+    return render(request,'signup.html')
+
+def login(request):
+    return render(request,'login.html')
+
+def about(request):
+    return render(request,'about.html')
+
+def contact(request):
+    return render(request,'contact.html')
+
+def bloggrid(request):
+    return render(request,'blog-grid.html')
+
+def buy(request):
+    return render(request,'buy.html')
+
+def sell(request):
+    return render(request,'sell.html')
+
+def update(request):
+    return render(request,'update.html')
+
+def rent(request):
+    return render(request,'rent.html')
